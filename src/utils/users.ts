ca59c0ab41fee2e75afc64cf6929fbc5706b3d3a
Kapil <kapildevtech5@gmail.com>
2026-02-03 15:20:38 +0530

    feat: added readme and example env

--- src/utils/users.ts
+++ src/utils/users.ts
@@ -5,12 +5,14 @@ export const users: User[] = [
   {
     id: 'u_admin',
     email: 'admin@example.com',
+    // password: 'admin123'
     passwordHash: bcrypt.hashSync('admin123', 10),
     role: 'admin',
   },
   {
     id: 'u_user',
     email: 'user@example.com',
+    // password: 'user123'
     passwordHash: bcrypt.hashSync('user123', 10),
     role: 'user',
   },
